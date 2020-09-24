227b853486a127e5471f4da161f1a820d9470633
CXY <576555730@qq.com>
2020-09-24 16:30:58 +0800

    modify request&response

--- src/cn/how2j/diytomcat/http/Response.java
+++ src/cn/how2j/diytomcat/http/Response.java
@@ -10,7 +10,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.Collection;
 import java.util.Locale;
 
-public class Response implements HttpServletResponse {
+public class Response extends BaseResponse {
 	
 	private StringWriter stringWriter;
 	private PrintWriter writer;
@@ -23,92 +23,22 @@ public class Response implements HttpServletResponse {
 		this.contextType = "text/html";
 	}
 
-	@Override
-	public String getCharacterEncoding() {
-		return null;
-	}
-
-	@Override
-	public String getContentType() {
-		return null;
-	}
-
-	@Override
-	public ServletOutputStream getOutputStream() throws IOException {
-		return null;
-	}
-
 	public PrintWriter getWriter(){
 		return this.writer;
-	}
-
-	@Override
-	public void setCharacterEncoding(String s) {
-
-	}
-
-	@Override
-	public void setContentLength(int i) {
-
-	}
-
-	@Override
-	public void setContentType(String s) {
-
-	}
-
-	@Override
-	public void setBufferSize(int i) {
-
-	}
-
-	@Override
-	public int getBufferSize() {
-		return 0;
-	}
-
-	@Override
-	public void flushBuffer() throws IOException {
-
-	}
-
-	@Override
-	public void resetBuffer() {
-
-	}
-
-	@Override
-	public boolean isCommitted() {
-		return false;
-	}
-
-	@Override
-	public void reset() {
-
-	}
-
-	@Override
-	public void setLocale(Locale locale) {
-
-	}
-
-	@Override
-	public Locale getLocale() {
-		return null;
 	}
 
 	public String getContextType(){
 		return this.contextType;
 	}
-	
+
 	public void setContextType(String contextType){
 		this.contextType = contextType;
 	}
-	
+
 	public void setBody(byte[] bytes){
 		this.body = bytes;
 	}
-	
+
 	public byte[] getBody()throws UnsupportedEncodingException {
 		if(this.body == null){
 			String content = stringWriter.toString();
@@ -117,109 +47,4 @@ public class Response implements HttpServletResponse {
 		return this.body;
 	}
 
-
-	@Override
-	public void addCookie(Cookie cookie) {
-
-	}
-
-	@Override
-	public boolean containsHeader(String s) {
-		return false;
-	}
-
-	@Override
-	public String encodeURL(String s) {
-		return null;
-	}
-
-	@Override
-	public String encodeRedirectURL(String s) {
-		return null;
-	}
-
-	@Override
-	public String encodeUrl(String s) {
-		return null;
-	}
-
-	@Override
-	public String encodeRedirectUrl(String s) {
-		return null;
-	}
-
-	@Override
-	public void sendError(int i, String s) throws IOException {
-
-	}
-
-	@Override
-	public void sendError(int i) throws IOException {
-
-	}
-
-	@Override
-	public void sendRedirect(String s) throws IOException {
-
-	}
-
-	@Override
-	public void setDateHeader(String s, long l) {
-
-	}
-
-	@Override
-	public void addDateHeader(String s, long l) {
-
-	}
-
-	@Override
-	public void setHeader(String s, String s1) {
-
-	}
-
-	@Override
-	public void addHeader(String s, String s1) {
-
-	}
-
-	@Override
-	public void setIntHeader(String s, int i) {
-
-	}
-
-	@Override
-	public void addIntHeader(String s, int i) {
-
-	}
-
-	@Override
-	public void setStatus(int i) {
-
-	}
-
-	@Override
-	public void setStatus(int i, String s) {
-
-	}
-
-	@Override
-	public int getStatus() {
-		return 0;
-	}
-
-	@Override
-	public String getHeader(String s) {
-		return null;
-	}
-
-	@Override
-	public Collection<String> getHeaders(String s) {
-		return null;
-	}
-
-	@Override
-	public Collection<String> getHeaderNames() {
-		return null;
-	}
 }
--- src/cn/how2j/diytomcat/webappServlet/HelloServlet.java
+++ src/cn/how2j/diytomcat/webappServlet/HelloServlet.java
@@ -1,5 +1,7 @@
 package cn.how2j.diytomcat.webappServlet;
 
+import cn.hutool.log.LogFactory;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +11,7 @@ public class HelloServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response){
         try{
+            LogFactory.get().error("response is:"+response);
             response.getWriter().println("Hello DIY Tomcat from HelloServlet");
         }catch (IOException e){
             e.printStackTrace();
